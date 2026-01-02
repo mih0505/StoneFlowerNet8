5c0c77b0c27983d5b28cf416e4ce2d034c6dd03e
mih0505 <mih0505@gmail.com>
2026-01-02 15:10:02 +0500

    Fix: remove nullable annotations 2

--- DataAccessLayer/StoneFlowersDbContext.cs
+++ DataAccessLayer/StoneFlowersDbContext.cs
@@ -21,35 +21,34 @@ public class StoneFlowersDbContext : IdentityDbContext<User, Role, Guid>, IStone
     : base(options)
     { }
 
-    public DbSet<Department> Departments { get; set; } = null!;
-    public DbSet<Organization> Organizations { get; set; } = null!;
-
-    public DbSet<Customer> Customers { get; set; } = null!;
-    public DbSet<Order> Orders { get; set; } = null!;
-    public DbSet<OrderProduct> OrderProducts { get; set; } = null!;
-    public DbSet<OrderType> OrderTypes { get; set; } = null!;
-    public DbSet<Payment> Payments { get; set; } = null!;
-    public DbSet<Category> Categories { get; set; } = null!;
-    public DbSet<CategoryProperty> CategoryProperties { get; set; } = null!;
-    public DbSet<Product> Products { get; set; } = null!;
-    public DbSet<ProductParameter> ProductParameters { get; set; } = null!;
-    public DbSet<ProductProperty> Properties { get; set; } = null!;
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Organization> Organizations { get; set; } 
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderProduct> OrderProducts { get; set; }
+    public DbSet<OrderType> OrderTypes { get; set; } 
+    public DbSet<Payment> Payments { get; set; } 
+    public DbSet<Category> Categories { get; set; } 
+    public DbSet<CategoryProperty> CategoryProperties { get; set; } 
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductParameter> ProductParameters { get; set; } 
+    public DbSet<ProductProperty> Properties { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
+        
         new DepartmentConfiguration().Configure(modelBuilder.Entity<Department>());
         new OrganizationConfiguration().Configure(modelBuilder.Entity<Organization>());
-
+               
         new CustomerConfiguration().Configure(modelBuilder.Entity<Customer>());
         new DeceasedConfiguration().Configure(modelBuilder.Entity<Deceased>());
         new OrderConfiguration().Configure(modelBuilder.Entity<Order>());
         new OrderProductConfiguration().Configure(modelBuilder.Entity<OrderProduct>());
         new OrderTypeConfiguration().Configure(modelBuilder.Entity<OrderType>());
         new PaymentConfiguration().Configure(modelBuilder.Entity<Payment>());
-
+        
         new CategoryConfiguration().Configure(modelBuilder.Entity<Category>());
         new CategoryPropertyConfiguration().Configure(modelBuilder.Entity<CategoryProperty>());
         new ProductConfiguration().Configure(modelBuilder.Entity<Product>());
